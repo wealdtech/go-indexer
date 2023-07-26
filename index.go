56b5fdf00fe7424c61d44b4ccc6f6903f177269b
Jim McDonald <Jim@mcdee.net>
2023-07-26 08:47:36 +0100

    Update dependencies.

--- index.go
+++ index.go
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package indexer provides an indexing system between names and UUIDs.
 package indexer
 
 import (
